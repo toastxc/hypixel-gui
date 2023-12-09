0da6e545f33b9142bc7147535dc21e970ffab768
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-12-09 22:25:21 +0800

    uploaded install script

---
+++ automation/install.sh
@@ -0,0 +1,18 @@
+# compile packages
+git clone https://github.com/toastxc/hypixel-gui.git
+cd hypixel-gui
+cargo b -r
+
+sudo cp ./target/release/hypixel-gui /bin/hypixel-gui
+
+# install application
+echo "[Desktop Entry]
+      Name=Hypixel
+      Exec=/bin/hypixel-gui
+      Icon=/path/to/your/icon.png
+      Type=Application
+      Categories=Utility;
+" > /usr/share/applications/hypixel-gui.desktop
+
+# update desktop
+sudo update-desktop-database
